b1610ea49620a6e30640d235b4670169ec301b41
SC_KPC_04 <SC_KPC_04@KSHRD-KPC-04s-Mac-mini.local>
2018-11-06 15:18:17 +0700

    Project

---
+++ app/src/main/java/com/example/sc_kpc_04/github_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sc_kpc_04.github_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
